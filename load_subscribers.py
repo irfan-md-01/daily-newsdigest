e4a01f2c16ff7b8b10afcc2500eb19962f12933b
Irfan Mohammad <145257683+irfan-md-01@users.noreply.github.com>
2025-06-08 14:08:49 +0530

    Update load_subscribers.py fixing bug

--- load_subscribers.py
+++ load_subscribers.py
@@ -1,17 +1,21 @@
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+# from oauth2client.service_account import ServiceAccountCredentials
 
 def get_subscriber():
-    scope = [
-        "https://spreadsheets.google.com/feeds",
-        "https://www.googleapis.com/auth/spreadsheets",
-        "https://www.googleapis.com/auth/drive"
-    ]
+    # scope = [
+    #     "https://spreadsheets.google.com/feeds",
+    #     "https://www.googleapis.com/auth/spreadsheets",
+    #     "https://www.googleapis.com/auth/drive"
+    # ]
 
-    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
-    client = gspread.authorize(creds)
+    # creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+    # client = gspread.authorize(creds)
 
-    sheet = client.open("Daily-NewsDigest Subscribers").sheet1
+    # sheet = client.open("Daily-NewsDigest Subscribers").sheet1
 
-    emails = sheet.col_values(2)[1:]
+    # emails = sheet.col_values(2)[1:]
+    # return tuple(set(emails))
+    gc = gspread.service_account(filename = "credentials.json")
+    sh = gc.open("Daily-NewsDigest Subscribers")
+    emails = sh.sheet1.col_values(2)[1:]
     return tuple(set(emails))
